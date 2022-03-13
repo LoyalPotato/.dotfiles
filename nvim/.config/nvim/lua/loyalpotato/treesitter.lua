dacbbf8b77590f2ebb801659c87e834e676933c7
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2022-03-13 22:55:15 +0000

    Config setup for treesitter

---
+++ nvim/.config/nvim/lua/loyalpotato/treesitter.lua
@@ -0,0 +1,10 @@
+require('nvim-treesitter.configs').setup({
+  ensure_installed = "maintained",
+  sync_install = false,
+  indent = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+  }
+})
