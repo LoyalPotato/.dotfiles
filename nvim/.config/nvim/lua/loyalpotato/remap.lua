6f806224e0b81da45dddd929f42ba92592ab56b3
LoyalPotato <david.bazilio.r@gmail.com>
2023-12-11 15:20:28 +0000

    Add keymap to telescope

--- nvim/.config/nvim/lua/loyalpotato/remap.lua
+++ nvim/.config/nvim/lua/loyalpotato/remap.lua
@@ -29,7 +29,8 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 ]]
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Don't remember what this does... but it's conflicting with some other one
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Use this to change perms easily
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
